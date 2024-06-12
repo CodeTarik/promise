a289d3253cc29e4b36762d671e0ed631fa03d2ab
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-06-12 22:16:10 +0200

    try catch 240612

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 let promError = false;
 
 function getPromise() {
-    return myPromise = new Promise((resolve, reject) => {
-          if(promError) {
-            reject("hat nicht geklappt");
-          }else{
-            resolve("hat geklappt");
-          }
-      });
+  return myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("1")
+    if(promError) {
+    reject("hat nicht geklappt");
+    }else{
+    resolve("hat geklappt");
+    }
+    }, 100);
+    });
+}
+
+function getPromise2() {
+  return myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("2")
+    if(!promError) {
+    reject("hat nicht geklappt");
+    }else{
+    resolve("hat geklappt");
+    }
+    }, 100);
+    });
+}
+
+function getPromise3() {
+  return myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if(promError) {
+    reject("hat nicht geklappt");
+    }else{
+    resolve("hat geklappt");
+    }
+    }, 100);
+    });
 }
 
 async function usePromise() {
+  
   try{
-    let prom = await getPromise();
-    console.log(prom)
-    console.log("prom2");
-    console.log("prom");
+    await getPromise();
+    await getPromise2();
+    await getPromise3();
+    console.log("juhu")
   } catch (error) {
-    console.error(error);
+    console.log(error)
   }
+    console.log("ende");
 
 }
 
